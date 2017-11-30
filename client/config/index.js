42f35e2eddecb1d7a6cb5133c072808a7ecf0454
Marcin Walaszek <walaszek.marcin@gmail.com>
2017-11-30 23:33:08 +0100

    fixed properties

--- client/config/index.js
+++ client/config/index.js
@@ -1,7 +1,7 @@
 const emailRegex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 
 module.exports = {
-    jalgoarenaApiUrl: "https://jalgo-api.herokuapp.com",
+    jalgoarenaApiUrl: "https://api:5001",
     title: "Start to solve your first problem",
     emailRegex,
     emailErrorMessage: "Please enter a valid email address",
